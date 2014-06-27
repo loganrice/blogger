e70d1ec35b1b2a2b76c05e1a1e3c7338cab7ca22
Logan Rice <loganrice72@gmail.com>
2014-06-27 16:01:55 -0700

    test 9

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-var taskTable;
+var blogTable;
 
 $(document).ready(function(){
-	var appKey = 'ca2w8rewh811swk';
+	var appKey = 'ji7obe86xh5d1nf';
 	var client = new Dropbox.Client({key: appKey});
 	// Try to finish OAuth authorization.
 	client.authenticate({interactive: false}, function (error) {
@@ -28,7 +28,7 @@ $(document).ready(function(){
 
 	    // Now you have a datastore. The next few examples can be included here.
 	    // define table name
-	    taskTable = datastore.getTable('tasks');
+	    blogTable = datastore.getTable('tasks');
 	});
 
 	// reads all tasks and adds them to html
@@ -41,22 +41,24 @@ $(document).ready(function(){
 
 var addTask = function(){
 	// add a task
-    var firstTask = taskTable.insert({
-				    taskname: 'Buy bread',
-				    completed: false,
-				    created: new Date()
+    var firstblog = blogTable.insert({
+				    created: new Date(),
+				    title: "Rails is Omakase",
+					excerpt: "some excerpt Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur voluptatibus nam quae in. Ad, tempore sed optio nulla animi natus laborum corporis, numquam consequuntur culpa eos ullam ex reprehenderit inventore.",
+					body: "some body Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur voluptatibus nam quae in. Ad, tempore sed optio nulla animi natus laborum corporis, numquam consequuntur culpa eos ullam ex reprehenderit inventore."
+
 				});
     // show task name in list
-    var taskname = firstTask.get('taskname');
+    var taskname = firstblog.get('taskname');
     $('#tasks').append("<li class='task'>" + taskname + "</li>");
-    var results = taskTable.query();
+    var results = blogTable.query();
 	for(var i = 0; i < results.length; i++){
 		$('#tasks').append("<li class='task'>" + results[i].getId() + "</li>");
 	};
 }
 
 // var taskQuery = function(){
-// 	var results = taskTable.query();
+// 	var results = blogTable.query();
 // 	for(i; i < results.length; i++){
 // 		$('#tasks').append("<li class='task'>" + results[i] + "</li>");
 // 	};
