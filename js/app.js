3a876c07f7616cafe95dae663d9aaa462f25e4fd
Logan Rice <loganrice72@gmail.com>
2014-06-29 16:05:02 -0700

    removed most functions from document ready func

--- js/app.js
+++ js/app.js
@@ -1,47 +1,31 @@
 var blogTable;
 var results;
 
-$(document).ready(function(){
-	var appKey = 'ji7obe86xh5d1nf';
-	var client = new Dropbox.Client({key: appKey});
-	// Try to finish OAuth authorization.
-	client.authenticate({interactive: false}, function (error) {
-	    if (error) {
-	        alert('Authentication error: ' + error);
-	    }
-	});
+var appKey = 'ji7obe86xh5d1nf';
+var client = new Dropbox.Client({key: appKey});
+// Try to finish OAuth authorization.
+client.authenticate({interactive: false}, function (error) {
+    if (error) {
+        alert('Authentication error: ' + error);
+    }
+});
 
-	if (client.isAuthenticated()) {
-	    // Client is authenticated. Display UI.
-	    alert("cool it worked");
-	}
+if (client.isAuthenticated()) {
+    // Client is authenticated. Display UI.
+    alert("cool it worked");
+}
 
-	//authenticate button
-	$('.btn').click(function(){
-		client.authenticate();
-	});
+var datastoreManager = client.getDatastoreManager();
 
-	var datastoreManager = client.getDatastoreManager();
-	datastoreManager.openDefaultDatastore(function (error, datastore) {
-	    if (error) {
-	        alert('Error opening default datastore: ' + error);
-	    }
+datastoreManager.openDefaultDatastore(function (error, datastore) {
+    if (error) {
+        alert('Error opening default datastore: ' + error);
+    }
 
-	    // Now you have a datastore. The next few examples can be included here.
-	    // define table name
-	    blogTable = datastore.getTable('tasks');
-	    results = blogTable.query();
-	    blogList(results);
-	});
-
-	
-
-	$("tbody").on("click", "post", function() {
-		var postTitle = $(this).text();
-		var post = blogTable.query( {title: postTitle});
-		$("#post-content").append(post[0].get('body'));
-	});
-	
+    // Now you have a datastore. The next few examples can be included here.
+    // define table name
+    blogTable = datastore.getTable('tasks');
+    results = blogTable.query();
 });
 
 function blogList(results) {
@@ -50,4 +34,21 @@ function blogList(results) {
 		$('#blog-table tr:last').append("<tr><td><a class='post' href='#'" + record.get('title') + "</a></td></tr>");
 		};
 };
+
+blogList(results);
+
+$(document).ready(function(){
+	//authenticate button
+	// $('.btn').click(function(){
+	// 	client.authenticate();
+	// });
+
+	$("tbody").on("click", ".post", function() {
+		var postTitle = $(this).text();
+		var post = blogTable.query( {title: postTitle});
+		$("#post-content").append(post[0].get('body'));
+	});
+});
+
+
 
