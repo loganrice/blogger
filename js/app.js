b1f582c8d5af697e1f870fa74e9e87d427b9b7ff
Logan Rice <loganrice72@gmail.com>
2014-06-29 16:34:33 -0700

    structure test1

--- js/app.js
+++ js/app.js
@@ -3,48 +3,50 @@ var results;
 
 var appKey = 'ji7obe86xh5d1nf';
 
+
+
+var client = new Dropbox.Client({key: appKey});
+// Try to finish OAuth authorization.
+client.authenticate({interactive: false}, function (error) {
+    if (error) {
+        alert('Authentication error: ' + error);
+    }
+});
+
+if (client.isAuthenticated()) {
+    // Client is authenticated. Display UI.
+    alert("cool it worked");
+}
+
+var datastoreManager = client.getDatastoreManager;
+
+datastoreManager.openDefaultDatastore( function (error, datastore) {
+    if (error) {
+        alert('Error opening default datastore: ' + error);
+    }
+
+    // Now you have a datastore. The next few examples can be included here.
+    // define table name
+    blogTable = datastore.getTable('tasks');
+    results = blogTable.query();
+});
+
+function blogList(results) {
+	for(var i = 0; i < results.length; i++){
+		var record = results[i];
+		$('#blog-table tr:last').append("<tr><td><a class='post' href='#'" + record.get('title') + "</a></td></tr>");
+		};
+};
+
+blogList(results);
+
+
+//authenticate button
+// $('.btn').click(function(){
+// 	client.authenticate();
+// });
+
 $(document).ready(function(){
-
-	var client = new Dropbox.Client({key: appKey});
-	// Try to finish OAuth authorization.
-	client.authenticate({interactive: false}, function (error) {
-	    if (error) {
-	        alert('Authentication error: ' + error);
-	    }
-	});
-
-	if (client.isAuthenticated()) {
-	    // Client is authenticated. Display UI.
-	    alert("cool it worked");
-	}
-
-	var datastoreManager = client.getDatastoreManager;
-
-	datastoreManager.openDefaultDatastore( function (error, datastore) {
-	    if (error) {
-	        alert('Error opening default datastore: ' + error);
-	    }
-
-	    // Now you have a datastore. The next few examples can be included here.
-	    // define table name
-	    blogTable = datastore.getTable('tasks');
-	    results = blogTable.query();
-	});
-
-	function blogList(results) {
-		for(var i = 0; i < results.length; i++){
-			var record = results[i];
-			$('#blog-table tr:last').append("<tr><td><a class='post' href='#'" + record.get('title') + "</a></td></tr>");
-			};
-	};
-
-	blogList(results);
-
-
-	//authenticate button
-	// $('.btn').click(function(){
-	// 	client.authenticate();
-	// });
 
 	$("tbody").on("click", ".post", function() {
 		var postTitle = $(this).text();
