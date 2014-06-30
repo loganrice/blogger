390db488bc967102ec0dd9e5fabc51ff88facb96
Logan Rice <loganrice72@gmail.com>
2014-06-29 17:06:58 -0700

    rev 9

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ $(function() {
 
 	    // Now you have a datastore. The next few examples can be included here.
 	    // define table name
-	    blogTable = datastore.getTable('tasks');
+	    blogTable = datastoreManager.getTable('tasks');
 	    results = blogTable.query();
 	});
 
